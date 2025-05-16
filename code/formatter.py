2dc48da7217f056fc002258e85f9dbc33d2a5c9f
RisingSatalite <saihajmann07@gmail.com>
2025-05-16 10:50:44 -0600

    Modified code/formatter.py
    - Modified files: code/formatter.py

--- code/formatter.py
+++ code/formatter.py
@@ -1,12 +1,34 @@
 import gradio as gr
 import os
 
-def greet(name, intensity):
-    return "Hello, " + name + "!" * int(intensity)
+def greet(name):
+    updated = ""
+    capitalize = True
+    spaced = False
+    for i in name:
+        if(i == ' ' and spaced == False):
+            spaced = True
+            capitalize = True
+            updated += i
+        elif(i == ' ' and spaced == True):
+            capitalize = True
+            pass
+        elif(i.isalpha() == False):
+            updated += i
+            spaced = False
+        elif(capitalize == True):
+            updated += i.upper()
+            capitalize = False
+            spaced = False
+        else:
+            updated += i.lower()
+            spaced = False
+    
+    return updated
 
 demo = gr.Interface(
     fn=greet,
-    inputs=["text", "slider"],
+    inputs=["text"],
     outputs=["text"],
 )
 
